192e32794ff8dfad8715823b8b3f2395df0e23c5
“junedzaman” <junedzaman89@gmail.com>
2023-01-16 04:42:11 -0500

    Class 5 homwwork part 2

---
+++ src/hw5Q3Constructor/Student.java
@@ -0,0 +1,22 @@
+package hw5Q3Constructor;
+
+public class Student {
+	public String stName;
+	public int stID;
+	public char sex;
+	public boolean isProgrammer;
+	public float grade;
+	
+	public Student() {
+		System.out.println("This is from the default Constructor of Student class.");
+	}
+
+	public Student(String stName, int stID, char sex,  float grade, boolean isProgrammer) {
+		this.stName = stName;
+		this.stID = stID;
+		this.sex = sex;
+		this.grade = grade;
+		this.isProgrammer = isProgrammer;
+		System.out.println("Student Name: "+stName+", ID: "+stID+", Sex: "+sex+", Grade: "+grade+" and Java Programmer? Ans: "+isProgrammer);
+	}
+}
---
+++ src/hw5Q3Constructor/StudentTest.java
@@ -0,0 +1,8 @@
+package hw5Q3Constructor;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		Student student2 = new Student("Juned Zaman", 54875, 'M', 3.457F, true);
+	}
+}
